30c384fc3138cdf9dc29164e4ee5a8f95e139ed9
Preston <childrep@vt.edu>
2016-05-28 19:40:14 -0400

    Site update

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,7 +10,7 @@
   if (window.location.port === '') { // if production
     // Uncomment app.baseURL below and
     // set app.baseURL to '/your-pathname/' if running from folder in production
-    app.baseUrl = '/Website/';
+    app.baseUrl = '/';
   }
 
   app.displayInstalledToast = function () {
@@ -57,41 +57,13 @@
     // imports are loaded and elements have been registered
   });
 
-  // Main area's paper-scroll-header-panel custom condensing transformation of
-  // the appName in the middle-container and the bottom title in the bottom-container.
-  // The appName is moved to top and shrunk on condensing. The bottom sub title
-  // is shrunk to nothing on condensing.
-  window.addEventListener('paper-header-transform', function (e) {
-    var appName = Polymer.dom(document).querySelector('#mainToolbar .app-name');
-    var middleContainer = Polymer.dom(document).querySelector('#mainToolbar .middle-container');
-    var bottomContainer = Polymer.dom(document).querySelector('#mainToolbar .bottom-container');
-    var detail = e.detail;
-    var heightDiff = detail.height - detail.condensedHeight;
-    var yRatio = Math.min(1, detail.y / heightDiff);
-    // appName max size when condensed. The smaller the number the smaller the condensed size.
-    var maxMiddleScale = 0.50;
-    var auxHeight = heightDiff - detail.y;
-    var auxScale = heightDiff / (1 - maxMiddleScale);
-    var scaleMiddle = Math.max(maxMiddleScale, auxHeight / auxScale + maxMiddleScale);
-    var scaleBottom = 1 - yRatio;
-
-    // Move/translate middleContainer
-    Polymer.Base.transform('translate3d(0,' + yRatio * 100 + '%,0)', middleContainer);
-
-    // Scale bottomContainer and bottom sub title to nothing and back
-    Polymer.Base.transform('scale(' + scaleBottom + ') translateZ(0)', bottomContainer);
-
-    // Scale middleContainer appName
-    Polymer.Base.transform('scale(' + scaleMiddle + ') translateZ(0)', appName);
-  });
-
   // Scroll page to top and expand header
   app.scrollPageToTop = function () {
-    app.$.headerPanelMain.scrollToTop(true);
+    app.$.headerPanelMain.scroll(top);
   };
 
   app.closeDrawer = function () {
-    app.$.paperDrawerPanel.closeDrawer();
+    app.$.appDrawer.resetLayout();
   };
 
   app.setTitle = function (event) {
@@ -104,10 +76,15 @@
     return x === y;
   };
 
-  app.formSubmit = function (event) {
-    event.localTarget.parentElement.submit();
+  app.formSubmit = function () {
+    app.$.contactForm.submit();
+    app.$.email.value = "";
+    app.$.name.value = "";
+    app.$.comments.value = "";
+    app.$.toast.text = 'Message sent!';
+    app.$.toast.show();
   };
-  
+
   app.selected = 0;
 
   app.calOptions = {
